5264f822a835ca1fc1713a42b7aa2271b84eaf73
tjfsteele <58075888+tjfsteele@users.noreply.github.com>
2024-01-06 14:01:26 +0000

    Add files via upload

--- software/wordclock/src/Iot.cpp
+++ software/wordclock/src/Iot.cpp
@@ -1,7 +1,6 @@
 #include "logging.h"
 #include "Iot.h"
 #include "Timezones.h"
-#include "nodo.h" // Nodo stuff
 
 #include <NeoPixelBus.h>
 #include <WiFi.h>
@@ -294,8 +293,8 @@ Iot::Iot(Display *display, RTC_DS3231 *rtc)
       timezone_param_(
           "Time zone", "timezone", timezone_value_, IOT_CONFIG_VALUE_LENGTH,
           "number", DEFAULT_TIMEZONE, DEFAULT_TIMEZONE, locationOptions),
-      // manual_date_param_("Date", "date", manual_date_value_, IOT_CONFIG_VALUE_LENGTH, "date",
-      //             "yyyy-mm-dd", nullptr, "pattern='\\d{4}-\\d{1,2}-\\d{1,2}'"),
+      //manual_date_param_("Date", "date", manual_date_value_, IOT_CONFIG_VALUE_LENGTH, "date",
+      //            "yyyy-mm-dd", nullptr, "pattern='\\d{4}-\\d{1,2}-\\d{1,2}'"),
       manual_time_param_("Time", "time", manual_time_value_, IOT_CONFIG_VALUE_LENGTH, "time",
                          "hh:mm:ss", nullptr,
                          "pattern='\\d{1,2}:\\d{1,2}:\\d{1,2}' step='1'"),
@@ -326,6 +325,7 @@ void Iot::updateClockFromParams_()
       parseColorValue(color_value_, RgbColor(255, 255, 255)));
   display_->setShowAmPm(parseBooleanValue(show_ampm_value_));
   display_->setSensorSentivity(parseNumberValue(ldr_sensitivity_value_, 0, 10, 5));
+
   if (parseBooleanValue(ntp_enabled_value_))
   {
     ntp_poll_timer_.start();
@@ -341,10 +341,8 @@ void Iot::updateClockFromParams_()
 void Iot::setup()
 {
   DCHECK(!initialized_, "[WARN] Trying to setup Iot multiple times.");
-  DLOGLN("starting iot::setup()");
-  ntp_poll_timer_.setup([this]()
-                        { maybeSetRTCfromNTP_(); },
-                        NTP_POLL_DELAY_SECONDS);
+
+  ntp_poll_timer_.setup([this]() { maybeSetRTCfromNTP_(); }, NTP_POLL_DELAY_SECONDS);
 
   // Required to properly trigger default values, due to a bug in IotWebConf.
   // show_ampm_value_[0] = '\0';
@@ -358,6 +356,7 @@ void Iot::setup()
   this->color_value_[0] = '\0';
   this->ntp_enabled_value_[0] = '\0';
   this->timezone_value_[0] = '\0';
+
   iot_web_conf_.setupUpdateServer(&http_updater_);
   iot_web_conf_.addParameter(&display_separator_);
   iot_web_conf_.addParameter(&show_ampm_param_);
@@ -366,20 +365,23 @@ void Iot::setup()
   iot_web_conf_.addParameter(&time_separator_);
   iot_web_conf_.addParameter(&ntp_enabled_param_);
   iot_web_conf_.addParameter(&timezone_param_);
-  // iot_web_conf_.addParameter(&manual_date_param_);
+  //iot_web_conf_.addParameter(&manual_date_param_);
   iot_web_conf_.addParameter(&manual_time_param_);
-  iot_web_conf_.setConfigSavedCallback([this]()
-                                       { handleConfigSaved_(); });
-  iot_web_conf_.setWifiConnectionCallback([this]()
-                                          { handleWifiConnection_(); });
+
+  iot_web_conf_.setConfigSavedCallback([this]() { handleConfigSaved_(); });
+
+  iot_web_conf_.setWifiConnectionCallback([this]() { handleWifiConnection_(); });
+
   iot_web_conf_.setHtmlFormatProvider(&customHtmlFormatProvider);
+
   iot_web_conf_.init();
+
   clearTransientParams_();
   updateClockFromParams_();
-  web_server_.on("/", [this]
-                 { handleHttpToRoot_(); });
-  web_server_.onNotFound([this]
-                         { iot_web_conf_.handleNotFound(); });
+
+  web_server_.on("/", [this] { handleHttpToRoot_(); });
+  web_server_.onNotFound([this] { iot_web_conf_.handleNotFound(); });
+
   initialized_ = true;
 }
 
@@ -420,8 +422,9 @@ void Iot::maybeSetRTCfromNTP_()
     return;
   }
 #ifdef LED
-  digitalWrite(LED, HIGH);
+  else digitalWrite(LED, HIGH);
 #endif
+
   rtc_->adjust(DateTime(timeinfo.tm_year, timeinfo.tm_mon, timeinfo.tm_mday, timeinfo.tm_hour, timeinfo.tm_min, timeinfo.tm_sec));
   DLOG("RTC set to:");
   DLOGLN(&timeinfo, "%A, %B %d %Y %H:%M:%S");
@@ -467,8 +470,6 @@ void Iot::handleHttpToRoot_()
 void Iot::handleConfigSaved_()
 {
   DLOGLN("Configuration was updated.");
-  DLOGLN("about to updateclockfromparams() after configuration was updated");
-
   updateClockFromParams_();
 }
 
