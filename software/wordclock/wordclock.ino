5b4651aff91231534c4ccf6d25dedb7c03ecc3bd
tjfsteele <58075888+tjfsteele@users.noreply.github.com>
2024-01-06 14:13:57 +0000

    Add files via upload

--- software/wordclock/wordclock.ino
+++ software/wordclock/wordclock.ino
@@ -1,30 +1,32 @@
-// Define debug to turn on debug logging.
-// #define DEBUG 1
 #include <Arduino.h>
 #include <RTClib.h>
-
 #include "src/logging.h"
 #include "src/Display.h"
 #include "src/ClockFace.h"
 #include "src/Iot.h"
-
+#include "src/nodo.h" // Nodo stuff
 RTC_DS3231 rtc;
-EnglishClockFace clockFace(ClockFace::LightSensorPosition::Bottom);
-// FrenchClockFace clockFace(ClockFace::LightSensorPosition::Bottom);
+
+EnglishClockFace clockFace(ClockFace::LightSensorPosition::Top);
+// FrenchClockFace clockFace(ClockFace::LightSensorPosition::Top);
+// DutchClockFace clockFace(ClockFace::LightSensorPosition::Top);
 Display display(clockFace);
+
 Iot iot(&display, &rtc);
 
 void setup()
 {
   setupLogging();
-
-  bool result = rtc.begin();
+#ifdef LED
+  pinMode(LED, OUTPUT);
+#endif
+  Wire.begin(SDA, SCL);
+  bool result = rtc.begin(); // pins for RTC swapped over on V1.0
   DCHECK(result, "RTC didn't start");
   if (rtc.lostPower())
   {
     DCHECK("RTC lost power. Battery was removed ?");
   }
-
   display.setup();
   iot.setup();
 }
