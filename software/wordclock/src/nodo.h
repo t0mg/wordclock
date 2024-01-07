cb4e044ec238841e15ca5ded6e0a4b95870b685b
tjfsteele <58075888+tjfsteele@users.noreply.github.com>
2024-01-07 14:06:09 +0000

    Add files via upload

--- software/wordclock/src/nodo.h
+++ software/wordclock/src/nodo.h
@@ -1,13 +1,13 @@
-// #define NODO // nodo version uses different pins and LEDs are in a different order
+#define NODO // nodo version uses different pins and LEDs are in a different order
 // #define PICO // Old board define PICO, new board don't define it to select proper I/O pins
 #ifdef NODO
 #ifdef PICO // ESP32-PICO
-#define SDA 22
-#define SCL 21
+#define SDA_PIN 22
+#define SCL_PIN 21
 #else // ESP32-C3
-#define SDA 6
-#define SCL 7
-#define LED 8
+#define SDA_PIN 6
+#define SCL_PIN 7
+#define LED_PIN 8
 #define LDR_PIN 1
 #define NEOPIXEL_PIN 0
 #endif
